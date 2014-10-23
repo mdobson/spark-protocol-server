ef53d7eb242e502e60d3d4829d6b0fb98e502bc6
Matt Dobson <mdobson4@gmail.com>
2014-10-23 07:23:15 -0400

    Updates to key gen untilities.

--- key.sh
+++ key.sh
@@ -3,6 +3,6 @@
 echo "Flashing core $1 with new keys for server at IP $2"
 spark keys new
 spark keys save $1
-
+./key_gen.js
 spark keys server default_public.pub.pem $2
 
--- key_gen.js
+++ key_gen.js
@@ -1,2 +1,19 @@
+#!/usr/bin/env node
+
+var ursa = require('ursa');
+var keyName = 'default_key.pem';
+
+
+if(!fs.existsSync(keyName)) {
+  console.log('creating new server key...');
+  var keys = ursa.generatePrivateKey();
+
+  var extensionIndex = keyName.lastIndexOf('.');
+  var derFilename = keyName.substring(0, extensionIndex) + '.der';
+  var pubPemFilename = keyName.substring(0, extensionIndex) + '.pub.pem';
+  fs.writeFileSync(keyName, keys.toPrivatePem('binary'));
+  fs.writeFileSync(pubPemFilename, keys.toPublicPem('binary'));
+}
+
 
 
